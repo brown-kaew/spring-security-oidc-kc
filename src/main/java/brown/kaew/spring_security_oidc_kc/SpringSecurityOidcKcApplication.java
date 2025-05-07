bdd75aeb562a157d761789fa4ba1d75adc6f5d97
brown-kaew <kaewtibkham@gmail.com>
2025-05-08 00:11:50 +0700

    Init project

---
+++ src/main/java/brown/kaew/spring_security_oidc_kc/SpringSecurityOidcKcApplication.java
@@ -0,0 +1,13 @@
+package brown.kaew.spring_security_oidc_kc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOidcKcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOidcKcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brown/kaew/spring_security_oidc_kc/SpringSecurityOidcKcApplicationTests.java
@@ -0,0 +1,13 @@
+package brown.kaew.spring_security_oidc_kc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOidcKcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
